813a9c9834b324781328627a9d1436371754ea82
Vincent Bitter <vincent@vincentbitter.nl>
2023-11-29 16:22:30 +0100

    Add reproduction

---
+++ server.js
@@ -0,0 +1,40 @@
+const crypto = require('node:crypto')
+const fastify = require('fastify')()
+const group = crypto.randomBytes(20).toString('hex')
+
+fastify
+  .register(require('@fastify/kafka'), {
+    producer: {
+      'metadata.broker.list': '127.0.0.1:9092',
+      'group.id': group,
+      'fetch.wait.max.ms': 10,
+      'fetch.error.backoff.ms': 50,
+      'dr_cb': true
+    },
+    consumer: {
+      'metadata.broker.list': '127.0.0.1:9092',
+      'group.id': group,
+      'fetch.wait.max.ms': 10,
+      'fetch.error.backoff.ms': 50,
+      'auto.offset.reset': 'earliest'
+    }
+  })
+
+fastify.post('/data', (req, reply) => {
+  fastify.kafka.push({
+    topic: 'updates',
+    payload: req.body,
+    key: 'dataKey'
+  })
+})
+
+fastify.kafka.subscribe('updates')
+fastify.kafka.on('updates', (msg, commit) => {
+  console.log(msg.value.toString())
+  commit()
+})
+
+fastify.listen({ port: 3000 }, err => {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+})
